2814f0f94f442ba033dda909f4a7c9b306ac50c6
Sheph3rd <novoa.shepard@gmail.com>
2015-12-12 17:50:48 +0100

    ChampionData Activity completada(sin botones)

--- app/src/main/java/com/wisdom/lol/lolwisdom/ChampionDataActivity.java
+++ app/src/main/java/com/wisdom/lol/lolwisdom/ChampionDataActivity.java
@@ -4,6 +4,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.webkit.WebView;
 
 public class ChampionDataActivity extends AppCompatActivity {
 
@@ -11,6 +12,7 @@ public class ChampionDataActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_champion_data);
+
     }
 
     @Override
